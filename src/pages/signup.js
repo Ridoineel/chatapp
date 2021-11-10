f7b6e74e9065e777f9e727f30a18c7223995cebd
ridoineel <ridoineelofficiel@gmail.com>
2021-11-10 15:47:30 +0000

    Replace content by component value

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,29 +1,9 @@
-
+import SignupForm from "./components/signupForm";
 
 function Signup() {
 
   return (
-    <div className="container col-6 mt-5">
-      <h2>Signup To ChapApp</h2>
-      <form className="border-sm">
-        <div className="form-group">
-          <input type="text" name="username" className="form-control" placeholder="Username" />
-          <small className="form-text text-muted">Just Test...</small>
-        </div>
-
-        <div className="form-group">
-          <input type="password" name="password" className="form-control" placeholder="Password" />
-          <small className="form-text text-muted">Just Test...</small>
-        </div>
-
-        <div className="form-group">
-          <input type="password" name="password-conf" className="form-control" placeholder="Password again" />
-          <small className="form-text text-muted">Just Test...</small>
-        </div>
-
-        <input type="submit" value="Signup" className="btn-pers col"/>
-      </form>
-    </div>
+      <SignupForm title={true} />
   )
 }
 
